04b0227a4f06bd79f4ba3fd8bec0220dbb1fa318
Jandir <jandirgregorio@icloud.com>
2026-03-01 18:04:15 -0500

    built server using deafult node:http

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/') {
+    res.writeHead(200, { 'content-type': 'text/plain'});
+    res.end('Welcome to JServer!');
+    return;
+  }
+  if (method === 'GET' && url === '/api/joke') {
+    const responseBody = JSON.stringify( { setup: 'Why did it rain?', punchline: 'Because'});
+    res.writeHead(200, { 'content-type': 'application/json'});
+    res.end(responseBody);
+    return;
+  }
+
+  const { pathname, searchParams } = new URL(url, 'http://localhost:8080');
+  if (method === 'GET' && pathname === '/api/rollDie') {
+    const quantity = parseInt(searchParams.get('quantity'));
+    const randomArr = Array.from({ length: quantity > 0 ? quantity : 1}, () => Math.floor(Math.random() * 6) + 1);
+    const responseBody = JSON.stringify({ rolls: randomArr});
+    res.writeHead(200, { 'content-type': 'application/json'});
+    res.end(responseBody);
+    return;
+  }
+  
+  res.writeHead(404, { 'content-type': 'application/json'});
+  res.end(JSON.stringify({ error: 'Not found'}));
+});
+
+server.listen(8080, () => {
+  console.log('Server listening on http://localhost:8080');
+});
