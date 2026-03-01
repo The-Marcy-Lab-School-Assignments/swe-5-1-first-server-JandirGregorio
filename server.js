b88a67c28c356b3e60d040fddaf8dd45345122ce
Jandir <jandirgregorio@icloud.com>
2026-03-01 18:58:27 -0500

    updated joke

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const http = require('node:http');
 
+const { pathname, searchParams } = new URL(url, 'http://localhost:8080');
 const server = http.createServer((req, res) => {
   const { method, url } = req;
   const timeStamp = new Date().toISOString();
   console.log(`${method} ${url} ${timeStamp}`);
+
   if (method === 'GET' && url === '/') {
     res.writeHead(200, { 'content-type': 'text/plain'});
     res.end('Welcome to JServer!');
     return;
   }
+
   if (method === 'GET' && url === '/api/joke') {
-    const responseBody = JSON.stringify( { setup: 'Why did it rain?', punchline: 'Because'});
+    const responseBody = JSON.stringify( { setup: 'Why did the bycicle fall over?', punchline: 'Because it was two tired.'});
     res.writeHead(200, { 'content-type': 'application/json'});
     res.end(responseBody);
     return;
   }
 
-  const { pathname, searchParams } = new URL(url, 'http://localhost:8080');
   if (method === 'GET' && pathname === '/api/rollDie') {
     const quantity = parseInt(searchParams.get('quantity'));
     const randomArr = Array.from({ length: quantity > 0 ? quantity : 1}, () => Math.floor(Math.random() * 6) + 1);
